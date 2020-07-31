f053bc46744bf31047477a331b5c949d9d387866
Linus Stoltz <lstoltz2@gmail.com>
2020-07-31 16:27:17 -0700

    integrate the functions

--- process_files.py
+++ process_files.py
@@ -5,30 +5,32 @@ import os, ntpath, uuid, re
 from pathlib import Path
 from datetime import datetime
 
-dataSource = r"C:\Users\lstol\Documents\Repositories\clean-data\inbox"
-datDestination = r"C:\Users\lstol\Documents\Repositories\clean-data\outbox"
+dataSource = r'C:\Users\lstol\Documents\Repositories\clean-data\inbox'
+datDestination = r'C:\Users\lstol\Documents\Repositories\clean-data\outbox'
 
 
 def findFiles():
     global dataSource
     for files in os.listdir(dataSource):
-        dataFiles = os.path.join(dataSource, files)
+        macFolders = os.path.join(dataSource, files)
 
-        processCSV(dataFiles)
-        cleanUpEmptyDir(dataFiles)
+        processCSV(macFolders)
+        cleanUpEmptyDir(macFolders)
 
-def processCSV(dataFiles):
-    for file in os.listdir(dataFiles):
-        filePath = os.path.join(dataFiles, file)
+def processCSV(macFolders):
+    for file in os.listdir(macFolders):
+        filePath = os.path.join(macFolders, file)
         fileName, fileExtension = os.path.splitext(filePath)
-
+        
         if (fileExtension == ".csv"):
             getLoggerNumber(os.path.basename(fileName))
-            cleanData(fileName, dataFiles)
+            cleanData(filePath)
 
-def cleanData(fileName, dataFiles):
+def cleanData(filePath):
 # Process and drop rows conditionally
-    df = pd.read_csv(dataFiles)
+# Dropping based on % change from one temp obs to another??
+   
+    df = pd.read_csv(filePath)
     print(df)
 
 
@@ -41,10 +43,8 @@ def cleanUpEmptyDir(devicePath):
     if (len(os.listdir(devicePath)) == 0):
         os.rmdir(devicePath)
 
+def main():
+    findFiles()
 
-# file = r'C:\Users\lstol\Documents\Repositories\clean-data\inbox\04-ee-03-73-87-74\2002003_sxt_DissolvedOxygen.csv'
-# df = pd.read_csv(file)
-# print(len(df))
-# df = df[df['DO Temperature (C)'] < 10]
-# print(len(df))
-# df.to_csv(data_destination + 'test.csv', index = False)
+main()
+
