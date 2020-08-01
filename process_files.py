088fb1a7ab09d354f671730ff287d5deaa0b86d9
Linus Stoltz <lstoltz5@gmail.com>
2020-07-31 19:30:04 -0700

    adding gps check

--- process_files.py
+++ process_files.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 import glob
-import os, ntpath, uuid, re
+import os
 from pathlib import Path
-from datetime import datetime
+
 
 dataSource = r'C:\Users\lstol\Documents\Repositories\clean-data\inbox'
 datDestination = r'C:\Users\lstol\Documents\Repositories\clean-data\outbox'
@@ -25,14 +25,17 @@ def processCSV(macFolders):
         if (fileExtension == ".csv"):
             getLoggerNumber(os.path.basename(fileName))
             cleanData(filePath)
+        
+        #if (fileExtension == ".gps"):
+            # Check the gps file to make sure the locations are not NA,
+            # If they are NA, move all three files to "flagged" dir 
 
 def cleanData(filePath):
-# Process and drop rows conditionally
-# Dropping based on % change from one temp obs to another??
-   
     df = pd.read_csv(filePath)
     print(df)
-
+    # Process and drop rows conditionally
+    # Dropping based on % change from one temp obs to another??     
+    # Two steps, one to remove rows based on thresholds, next step removes 2 observations from both ends to gaurentee sensor is on the bottom 
 
 def getLoggerNumber(filename):
     global loggerNumber
@@ -42,6 +45,13 @@ def getLoggerNumber(filename):
 def cleanUpEmptyDir(devicePath):
     if (len(os.listdir(devicePath)) == 0):
         os.rmdir(devicePath)
+
+#def moveFiles(files):
+ #   for file in files:
+  #      filename = ntpath.basename(file)
+   #     folderStructure = file.split(os.path.sep)
+    #    Path(dataDestination + os.path.sep + folderStructure[-2] + os.path.sep).mkdir(parents=True, exist_ok=True)
+     #   os.rename(file, dataDestination + os.path.sep + folderStructure[-2] + os.path.sep + filename)
 
 def main():
     findFiles()
